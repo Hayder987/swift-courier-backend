af3399e22167849caa69ae45e7abae015fb0b404
Hayder Ali <hayderbd4290@gmail.com>
2026-09-02 00:17:01 +0600

    feat: create winston loger and use helmet, cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,39 @@
-import express, { Application } from "express";
-
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { requestLogger } from "./app/middleware/requestLogger";
+import config from "./app/config";
+import httpStatus from "http-status";
 
 
 const app:Application = express();
+
+// using helmet middleware
+app.use(
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+    },
+  }),
+);
+
+app.use(requestLogger);
+
+app.use(
+	cors({
+		origin: config.frontend_url,
+		credentials: true,
+	}),
+);
+
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+	res.status(httpStatus.OK).json({
+		success: true,
+		message: "Welcome to Swift Courier Services",
+	});
+});
 
 
 export default app;
---
+++ src/app/middleware/requestLogger.ts
@@ -0,0 +1,26 @@
+import type { NextFunction, Request, Response } from "express";
+
+import logger from "../utils/logger";
+
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const startTime = Date.now();
+
+  res.on("finish", () => {
+    const duration = Date.now() - startTime;
+
+    logger.info({
+      type: "HTTP_REQUEST",
+      method: req.method,
+      url: req.originalUrl,
+      statusCode: res.statusCode,
+      duration: `${duration}ms`,
+      ip: req.ip,
+    });
+  });
+
+  next();
+};
