d081eade87cfde050752fe39f88eeab654e45d48
Hayder Ali <hayderbd4290@gmail.com>
2026-09-06 00:42:48 +0600

    final prisma schema design done and add salary system

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import express, { type Application, type Request, type Response } from "express";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
@@ -9,25 +13,27 @@ import { apiRateLimiter } from "./app/middleware/apiRateLimiter";
 import router from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
+import { findZoneFromCoordinates } from "./app/utils/zone-utils/findZoneByCoordinates";
+import { prisma } from "./app/lib/prisma";
 
 const app: Application = express();
 
 // using helmet middleware
 app.use(
-	helmet({
-		crossOriginResourcePolicy: {
-			policy: "cross-origin",
-		},
-	}),
+  helmet({
+    crossOriginResourcePolicy: {
+      policy: "cross-origin",
+    },
+  }),
 );
 
 app.use(requestLogger);
 
 app.use(
-	cors({
-		origin: config.frontend_url,
-		credentials: true,
-	}),
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
 );
 
 // Enable URL-encoded form data parsing
@@ -42,12 +48,39 @@ app.use("/api", apiRateLimiter);
 // user module routes common
 app.use("/api/v1", router);
 
+app.get("/api/v1/test", async (req: Request, res: Response) => {
+
+  const zones = await prisma.zone.findMany({
+    where: {
+      isActive: true,
+    },
+    select: {
+      id: true,
+      name: true,
+      boundary: true,
+    },
+  });
+
+  const zone = findZoneFromCoordinates(
+    23.743307,
+    90.398808,
+    zones,
+  );
+
+
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Test Result",
+	data : zone
+  });
+});
+
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
-	res.status(httpStatus.OK).json({
-		success: true,
-		message: "Welcome to Swift Courier Services",
-	});
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to Swift Courier Services",
+  });
 });
 
 // using global Error
